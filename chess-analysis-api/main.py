1b28eb0dbdd922cf4e07d3579053e5c2fb75c0c2
Zatonix <theochelim@hotmail.fr>
2024-02-10 00:59:57 +0100

    :fire: Remove typo on URL cors origin

--- chess-analysis-api/main.py
+++ chess-analysis-api/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "https://chess-tactics-trainer.vercel.app/"
+    "https://chess-tactics-trainer.vercel.app"
 ]
 
 app.add_middleware(
