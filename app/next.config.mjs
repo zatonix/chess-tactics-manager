c8d695e1f5aacfdbe71ab40591584908521400d3
Zatonix <theochelim@hotmail.fr>
2024-02-25 12:49:21 +0100

    :fire: Remove next config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    NEXTAUTH_URL: 'https://chess-tactics-manager.vercel.app',
-    NEXTAUTH_SECRET: 'EH1+0rfxOQ4uGGeXvJvuaDKTUGLY+Sh1/6qdV8podUQ=',
-};
+const nextConfig = {};
 
 export default nextConfig;
