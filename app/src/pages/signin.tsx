02af839ebdf00215ffbe471cf3911a7cb35925f9
Zatonix <theochelim@hotmail.fr>
2024-02-26 21:08:09 +0100

    :lipstick: Responsive design for login page (#18)

--- app/src/pages/signin.tsx
+++ app/src/pages/signin.tsx
@@ -1,3 +1,4 @@
+import { nextAuthConfig } from "@/pages/api/auth/[...nextauth]"
 import type {
     GetServerSidePropsContext,
     InferGetServerSidePropsType,
@@ -5,44 +6,42 @@ import type {
 import { getServerSession } from "next-auth/next"
 import { getProviders, signIn } from "next-auth/react"
 import Image from 'next/image'
-import { nextAuthConfig } from "@/pages/api/auth/[...nextauth]"
 
 import '@/app/globals.css'
 import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
+import { Checkbox } from "@/components/ui/checkbox"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Checkbox } from "@/components/ui/checkbox"
+import { Separator } from "@/components/ui/separator"
 
-export const metadata = {
-    title: 'Chess Tactics Manager',
-    description: 'A great analysis application of chess tactics',
-}
+// export const metadata = {
+//     title: 'Chess Tactics Manager',
+//     description: 'A great analysis application of chess tactics',
+// }
 
 export default function SignIn({
     providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
         <>
-            <div className="font-sans w-full h-screen flex bg-foreground text-white">
-                <Image src="/login.jpg" alt='background' width={1920} height={1080} className="object-cover object-center h-screen w-7/12" />
-                <div className="flex flex-col justify-center items-center w-5/12 shadow-lg">
-                    <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            <div className="w-full min-h-screen bg-foreground flex text-white items-center justify-center border-none">
+                <Image src="/login.jpg" alt='background' width={1920} height={1080} className="object-cover object-center h-screen w-7/12 hidden lg:block" />
+                <div className="flex flex-col justify-around items-center w-full lg:w-5/12">
+                    <h1 className="scroll-m-20 border-b pb-2 mb-10 text-3xl font-semibold tracking-tight first:mt-0">
                         Chess Tactics Manager
                     </h1>
-                    <div className="w-1/2 text-left mt-12">
+                    <div className="w-1/2 text-left">
                         <Label htmlFor="email">Email</Label>
                         <Input type="text" name="username" placeholder="Type your Email address"
-                            className="shadow-md border mt-2 w-full h-10 px-3 py-2 bg-background border-background  mb-4 rounded-none" />
+                            className="shadow-md border mt-2 w-full h-10 px-3 py-2 bg-background border-background mb-4 rounded-none" />
                         <Label htmlFor="password">Password</Label>
                         <Input type="password" name="password" placeholder="Type your Password"
-                            className="shadow-md border mt-2 w-full h-10 px-3 py-2 bg-background border-background  mb-4 rounded-none" />
+                            className="shadow-md border mt-2 w-full h-10 px-3 py-2 bg-background border-background mb-4 rounded-none" />
                         <div className="flex flex-row justify-between text-xs mb-8">
                             <div className="flex justify-center items-center gap-2"><Checkbox checked /> Remerber me </div>
-                            <div>Forgot your password ?</div>
+                            <div>Forgot your password?</div>
                         </div>
                         <Button className="btn btn-primary dark mt-4 w-full rounded-none" size='lg'> Connexion </Button>
-
                         <Separator className="mt-12 mb-12" />
                         <div className="flex items-center justify-center flex-col gap-2">
                             <Button
@@ -55,13 +54,15 @@ export default function SignIn({
                             </Button>
                             <Button
                                 type="button"
-                                className="text-white bg-white w-full font-medium rounded-none bg-[#050708] hover:bg-[#050708]/80 text-sm px-5 py-6 text-center inline-flex items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 me-2 mb-2">
+                                className="text-white w-full font-medium rounded-none bg-[#050708] hover:bg-[#050708]/80 text-sm px-5 py-6 text-center inline-flex items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 me-2 mb-2">
 
                                 <svg className="w-5 h-5 me-2 -ms-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg>
                                 Continue with Apple
                             </Button>
                             <p className="text-sm mt-10"> Don’t have an account? <a className="text-primary">Sign up now</a> </p>
-                            <p className="text-xs mt-24"> © Chess Tactics Manager 2024 </p>
+                        </div>
+                        <div className="text-center mt-5">
+                            <p className="text-xs mt-10"> © Chess Tactics Manager 2024 </p>
                         </div>
                     </div>
                 </div>
