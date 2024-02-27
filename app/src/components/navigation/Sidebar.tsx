635b4eb6150e87a2f9a18d93a5e8ee68b5b7a8ad
Zatonix <theochelim@hotmail.fr>
2024-02-27 01:06:41 +0100

    :sparkles: Improve application routing

---
+++ app/src/components/navigation/Sidebar.tsx
@@ -0,0 +1,33 @@
+
+"use client"
+
+import React, { useMemo } from 'react'
+import Image from 'next/image'
+import { LayoutDashboard, AreaChart, UserCog } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import { SidebarButton } from './SidebarButton'
+
+export const Sidebar = () => {
+    const pathname = usePathname()
+
+    const activeMenu = useMemo(() => {
+        if (pathname?.startsWith('/stats')) {
+            return 'stats'
+        } else if (pathname?.startsWith('/profil')) {
+            return 'profile'
+        }
+        return 'home'
+    }, [pathname])
+
+    return (
+        <div className="flex flex-col min-w-20 w-20 justify-between items-center bg-foreground shadow-lg">
+            <Image src="/logo.png" width="310" height="310" alt="logo" className="w-12 m-4" />
+            <div className="flex flex-col items-center justify-around gap-12">
+                <SidebarButton label='Home' baseUrl='/' icon={<LayoutDashboard />} active={activeMenu === 'home'} />
+                <SidebarButton label='Stats' baseUrl='/stats' icon={<AreaChart />} active={activeMenu === 'stats'} />
+                <SidebarButton label='Profile' baseUrl='/profile' icon={<UserCog />} active={activeMenu === 'profile'} />
+            </div>
+            <p></p>
+        </div>
+    )
+}
