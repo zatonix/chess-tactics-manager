76e8f1c126785ef13127b5e8d147ff848d979c2f
Zatonix <theochelim@hotmail.fr>
2024-08-05 15:24:54 +0200

    :pencil: Fix linter

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -4,8 +4,10 @@ import { Providers } from './providers'
 
 export const metadata = {
   title: 'Chess Tactics Manager',
-  description: `Chess Tactics Manager is an open-source web application designed for training and analyzing chess tactics. 
-                This application provides a unified platform to track and improve your chess skills by integrating game data from popular chess platforms such as Lichess.org and Chess.com.`
+  description: `Chess Tactics Manager is an open-source web application designed 
+  for training and analyzing chess tactics. 
+  This application provides a unified platform to track and improve your chess skills by integrating game data 
+  from popular chess platforms such as Lichess.org and Chess.com.`
 }
 
 export default function RootLayout({
